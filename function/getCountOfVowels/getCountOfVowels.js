817a5b4f18280bd23d8faedc13ecf473b6b8d81b
Павел Егоров <pavel_egoroff@mail.ru>
2023-05-25 20:52:54 +0300

    Using a string instead of an array

--- function/getCountOfVowels/getCountOfVowels.js
+++ function/getCountOfVowels/getCountOfVowels.js
@@ -7,7 +7,7 @@
 export const getCountOfVowels = (str) => {
   if (!str) return 0;
   const lowerStr = str.toLowerCase();
-  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  const vowels = 'aeiou';
   let vowelCount = 0;
 
   for (const letter of lowerStr) {
