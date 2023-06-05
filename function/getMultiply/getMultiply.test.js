c63fca78a49fcd6380603e26e8aa4ee3dfd3ed28
Павел Егоров <pavel_egoroff@mail.ru>
2023-06-05 22:44:32 +0300

    Implemented getMultiply function
    Created tests
    
    Created getMultiply function
    
    Fixed

---
+++ function/getMultiply/getMultiply.test.js
@@ -0,0 +1,10 @@
+import {getMultiply} from './getMultiply.js';
+
+console.log(getMultiply('5', 5) === undefined);
+console.log(getMultiply(5, true) === undefined);
+console.log(getMultiply(-5, 5) === undefined);
+console.log(getMultiply(5, -5) === undefined);
+console.log(getMultiply(5, 0) === undefined);
+console.log(getMultiply(0, 5) === undefined);
+console.log(getMultiply(1.5, 5) === undefined);
+console.log(getMultiply(5, 5) === 25);
