06425b1b7ffd033f2bd85a083f36202fcd7e37a4
Palych18 <pavel_egoroff@mail.ru>
2023-11-16 23:38:43 +0300

    Refactored other files

--- functions/getCountOfVowels/getCountOfVowels.test.js
+++ functions/getCountOfVowels/getCountOfVowels.test.js
@@ -1,5 +1,13 @@
-import { getCountOfVowels } from './getCountOfVowels.js/';
+import { getCountOfVowels } from './getCountOfVowels.js';
 
-describe ('getCountOfVowels:', () => {
-  it('Unit test 1', () => { expect(getCountOfVowels('Audio')).toEqual(4); });
+describe('getCountOfVowels:', () => {
+  it('Unit test 1', () => {
+    const strVowels = getCountOfVowels('Audio');
+    expect(strVowels).toEqual(4);
+  });
+
+  it('Unit test 2', () => {
+    const strVowels = getCountOfVowels('JavaScript');
+    expect(strVowels).toEqual(3);
+  });
 });
